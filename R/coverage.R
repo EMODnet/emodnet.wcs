af4bf2f405365d45795cb5e0a7f68de1beedd48f
Maëlle Salmon <maelle.salmon@yahoo.se>
2025-09-12 14:50:17 +0200

    use phrasing from ows4R

--- R/coverage.R
+++ R/coverage.R
@@ -5,7 +5,9 @@
 #' @param bbox a named numeric vector of length 4, with names `xmin`, `ymin`,
 #' `xmax` and `ymax`. specifying the bounding box.
 #' (extent) of the raster to be returned.
-#' @param crs the CRS of the supplied bounding box. Defaults to `"EPSG:4326"`.
+#' @param crs the CRS of the supplied bounding box
+#' (EPSG prefixed code, or URI/URN).
+#' Defaults to `"EPSG:4326"`.
 #' @param time for coverages that include a temporal dimension,
 #' a vector of temporal coefficients specifying the
 #' time points for which coverage data should be returned.
